29d6de48e5941f5a660b3c7da3f8d24f5de254bf
Michael Alex Lynch <mlynch5187@gmail.com>
2020-12-15 12:34:47 -0700

    Add book list to app.py; add GET route for /books

--- server/app.py
+++ server/app.py
@@ -21,3 +21,28 @@ def ping_pong():
 
 if __name__ == '__main__':
     app.run()
+
+BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
+
+@app.route('/books', methods=['GET'])
+def all_books():
+    return jsonify({
+        'status': 'success',
+        'books': BOOKS
+    })
