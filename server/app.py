34ace129a833388756e63539f91923ec6b596a08
Michael Alex Lynch <mlynch5187@gmail.com>
2020-12-16 13:07:45 -0700

    Move Books seeds to app.py configuration

--- server/app.py
+++ server/app.py
@@ -5,23 +5,6 @@ from flask_cors import CORS
 # configuration
 DEBUG = True
 
-# instantiate the app
-app = Flask(__name__)
-app.config.from_object(__name__)
-
-# enable CORS
-CORS(app, resources={r'/*': {'origins': '*'}})
-
-
-# sanity check route
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify('pong!')
-
-
-if __name__ == '__main__':
-    app.run()
-
 BOOKS = [
     {
         'title': 'On the Road',
@@ -40,9 +23,25 @@ BOOKS = [
     }
 ]
 
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
+
 @app.route('/books', methods=['GET'])
 def all_books():
     return jsonify({
-        'status': 'success',
-        'books': BOOKS
+    'status': 'success',
+    'books': BOOKS
     })
+
+if __name__ == '__main__':
+    app.run()
