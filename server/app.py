10158b7b76aad08f2c4f9ba9fc7563fae0becb5d
Michael Alex Lynch <mlynch5187@gmail.com>
2020-12-16 14:33:00 -0700

    Add unique IDs to each new book

--- server/app.py
+++ server/app.py
@@ -1,21 +1,25 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import uuid
 
 # configuration
 DEBUG = True
 
 BOOKS = [
     {
+        'id': uuid.uuid4().hex,
         'title': 'On the Road',
         'author': 'Jack Kerouac',
         'read': True
     },
     {
+        'id': uuid.uuid4().hex,
         'title': 'Harry Potter and the Philosopher\'s Stone',
         'author': 'J. K. Rowling',
         'read': False
     },
     {
+        'id': uuid.uuid4().hex,
         'title': 'Green Eggs and Ham',
         'author': 'Dr. Seuss',
         'read': True
